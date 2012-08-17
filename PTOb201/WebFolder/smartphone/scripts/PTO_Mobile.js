73509d43583968d3acc9391a2e7bd44398962949
Xiang Liu <liuxiang625@gmail.com>
2012-08-17 15:56:08 -0700

    dtatprovide api added, pto hours and floating days caculation done

--- PTOb201/WebFolder/smartphone/scripts/PTO_Mobile.js
+++ PTOb201/WebFolder/smartphone/scripts/PTO_Mobile.js
@@ -1,37 +1,82 @@
 ﻿function button_clicked() {
     if (WAF.directory.loginByPassword($("#textinput1").val(), $("#textinput2").val())) {
-    	WAF.ds.User.all();
-        var requestCollection = WAF.sources.pTO_Request.getEntityCollection();
-        requestCollection.forEach({ // on each of the entity collection
-            onSuccess: function(event) {
-                var entity = event.entity; // get the entity from event.entity
-                var requestStatus = entity.status.getValue();
-                var ptoHours = entity.requestLineItemCollection.getRelatedClass;
-                if (requestStatus != "commit") {
-                   var $element = $('<div data-role="collapsible" data-collapsed="true" '
-                    + (entity.status.getValue() === "approved" ? 'data-theme="b"' : '') +'><h3>' 
-                    + formatDate(entity.firstDayOff.getValue()) + "- " 
-                    + formatDate(entity.lastDayOff.getValue())+ "  " 
-                    + requestStatus + '</h3><p>' 
-                    + "PTO hours:" + ptoHours + '</p><p>'
-                    + 'Return to work at: '+ formatDate(entity.returnToWorkDate.getValue()) +'</p>'
-                    + (entity.notes.getValue()?'<p> Notes: '+entity.notes.getValue() +'</p>':"")+
-                    + '</div>')
-                    .appendTo($('#collapsibleSet'));
-                    $element.collapsible();
-                }
-                }, onError: function(event) {
-                    $("#display").html("An error has been returned");
-                },
-                //            atTheEnd: function(event) {
-                //                $("#display").html(html); // display of the final result
-                //            },
-        });
-
-        function formatDate(date) {
-            return date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear()
-        }
-
+    	WAF.ds.User.query("fullName = :1",WAF.directory.currentUser().fullName , {
+    		autoExpand: "pTO_RequestCollection",
+			//orderBy: "fullName",
+    		onSuccess: function(event) {
+    			event.entityCollection.forEach({
+    				onSuccess: function(ev) {
+    					var ptoCollectionRel = ev.entity.pTO_RequestCollection.relEntityCollection;
+    					if (ptoCollectionRel.length > 0) {
+    						ptoCollectionRel.forEach({
+    							onSuccess: function(eventRelPTO) {
+    							var ptoRequest = eventRelPTO.entity;
+    							var requestStatus = ptoRequest.status.getValue();
+								var requestLineItems = ptoRequest.getLineItemsRange(ptoRequest.firstDayOff.getValue(), ptoRequest.returnToWorkDate.getValue());
+									if (requestStatus != "commit") {
+										var ptoHours = 0;
+										var floatingDays = 0; 
+										requestLineItems.forEach({
+											onSuccess: function(ev3) {
+												var theHours = ev3.entity.hoursRequested.getValue();
+												ptoHours += theHours;
+												(!theHours| ev3.entity.compensation.getValue().indexOf("Floating")? floatingDays += 1: floatingDays += 0);
+											}
+										});
+               							var $element = $('<div data-role="collapsible" data-collapsed="true" '
+                   						+ (requestStatus === "approved" ? 'data-theme="b"' : '') +'><h3>' 
+                   						+ formatDate(ptoRequest.firstDayOff.getValue()) + "- " 
+                  						+ formatDate(ptoRequest.lastDayOff.getValue())+ "  " 
+                   						+ requestStatus + '</h3><p>' 
+                   						+ "PTO hours:" + ptoHours + '</p><p>'
+                   						+ "Floating days:" + floatingDays + '</p><p>'
+                  						+ 'Return to work at: '+ formatDate(ptoRequest.returnToWorkDate.getValue()) +'</p>'
+                   						+ (ptoRequest.notes.getValue()?'<p> Notes: '+ptoRequest.notes.getValue() +'</p>':"")+
+                    						+ '</div>')
+                    						.appendTo($('#collapsibleSet'));
+                   						$element.collapsible();
+               						}
+									
+//									requestLineItems.forEach({
+//										onSuccess: function(ev3) {
+//											var theHours = ev3.entity.hoursRequested.getValue();
+//											alert(theHours);
+//										}
+//									});
+								}
+    						});
+    					}
+    				}
+    			});
+    		}
+    		
+    	});
+//        var requestCollection = WAF.sources.pTO_Request.getEntityCollection();
+//        requestCollection.forEach({ // on each of the entity collection
+//            onSuccess: function(event) {
+//                var entity = event.entity; // get the entity from event.entity
+//                var requestStatus = entity.status.getValue();
+//                var ptoHours = entity.requestLineItemCollection.getRelatedClass;
+//                if (requestStatus != "commit") {
+//                   var $element = $('<div data-role="collapsible" data-collapsed="true" '
+//                    + (entity.status.getValue() === "approved" ? 'data-theme="b"' : '') +'><h3>' 
+//                    + formatDate(entity.firstDayOff.getValue()) + "- " 
+//                    + formatDate(entity.lastDayOff.getValue())+ "  " 
+//                    + requestStatus + '</h3><p>' 
+//                    + "PTO hours:" + ptoHours + '</p><p>'
+//                    + 'Return to work at: '+ formatDate(entity.returnToWorkDate.getValue()) +'</p>'
+//                    + (entity.notes.getValue()?'<p> Notes: '+entity.notes.getValue() +'</p>':"")+
+//                    + '</div>')
+//                    .appendTo($('#collapsibleSet'));
+//                    $element.collapsible();
+//                }
+//                }, onError: function(event) {
+//                    $("#display").html("An error has been returned");
+//                },
+//                //            atTheEnd: function(event) {
+//                //                $("#display").html(html); // display of the final result
+//                //            },
+//        });
     }
     else {
         $('#log > div').remove();
@@ -39,7 +84,10 @@
         event.stopPropagation();
         $('<div></div>').append('Invalid username or password').appendTo('#log');
     }
-}
+};
+function formatDate(date) {
+	return date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear()
+};
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
